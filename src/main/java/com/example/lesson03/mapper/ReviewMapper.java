ad7d2313d9d12c0cb0e6a93a15d1b1610647bc5d
DESKTOP-BNIS1T3\user <test@test.com>
2024-06-14 18:48:15 +0900

    spring_240614

---
+++ src/main/java/com/example/lesson03/mapper/ReviewMapper.java
@@ -0,0 +1,15 @@
+package com.example.lesson03.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.example.lesson03.domain.Review;
+
+@Mapper
+public interface ReviewMapper {
+	
+	// input: id(int)
+	// output: Review(단건) or null
+	public Review selectReviewById(int id);
+	
+	
+}
